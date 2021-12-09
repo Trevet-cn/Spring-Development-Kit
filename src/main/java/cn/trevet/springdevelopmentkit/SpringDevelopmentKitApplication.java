db4d5eeeb0c77f2ef92955f3ad14c2c804a1a545
Trevet <727770491@qq.com>
2021-12-09 09:44:12 +0800

    初始化工具项目原型

---
+++ src/main/java/cn/trevet/springdevelopmentkit/SpringDevelopmentKitApplication.java
@@ -0,0 +1,13 @@
+package cn.trevet.springdevelopmentkit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDevelopmentKitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDevelopmentKitApplication.class, args);
+    }
+
+}
